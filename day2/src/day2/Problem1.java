189c0d1481a35b6c6d11e9ffa1894c8269a78898
pavanpawar4591 <pavanpawar4591@gmail.com>
2024-05-21 17:57:06 +0530

    problem 1 day 2

--- day2/src/day2/Problem1.java
+++ day2/src/day2/Problem1.java
@@ -1,10 +1,55 @@
 package day2;
 
+
+/*
+ * 
+ * 
+ * 
+ Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s. The maximum number of consecutive 1s is 3.
+Example 2:
+
+Input: nums = [1,0,1,1,0,1]
+Output: 2
+ */
 public class Problem1 {
 
 	public static void main(String[] args) {
 		System.out.println("day2 question 1");
+		int [] nums = {1,0,1,1,1,0,0};
+		System.out.println(findMaxConsecutiveOnes(nums));
+		
+		 int [] nums2 = {1,0,1,0,1,0,0,1,1};
+		System.out.println(findMaxConsecutiveOnes(nums2));
 
 	}
+	
+	  public static int findMaxConsecutiveOnes(int[] nums) {
+		     int count =0;
+		     int max =0;
+		     for(int i =0 ;i<nums.length;i++) 
+		     {
+		            if(nums[i]==1)
+		            {
+		                count++;
+		            }else{
+		                count = 0;
+		            }
+		            
+		           max = Math.max(max,count); 
+
+		     }  
+		     
+
+		     
+		     return max;
+		    }
 
 }
